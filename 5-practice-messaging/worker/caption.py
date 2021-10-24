0a0a1ec4d4149a74c798ad451cdba502f923c520
whiteRa2bit <pavel.fakanov@gmail.com>
2021-10-24 20:26:27 +0300

    Remove ml model

--- 5-practice-messaging/worker/caption.py
+++ 5-practice-messaging/worker/caption.py
@@ -1,22 +1,2 @@
-import torch
-import skimage.io as io
-import PIL.Image
-
-from model import model, preprocess, clip_model, tokenizer, PREFIX_LENGTH, DEVICE
-from generate import generate2, generate_beam
-
-
-def get_image_caption(image_url, prefix_length=PREFIX_LENGTH, use_beam_search=False):
-    image = io.imread(image_url)
-    pil_image = PIL.Image.fromarray(image)
-
-    image = preprocess(pil_image).unsqueeze(0).to(DEVICE)
-    with torch.no_grad():
-        prefix = clip_model.encode_image(image).to(DEVICE, dtype=torch.float32)
-        prefix_embed = model.clip_project(prefix).reshape(1, prefix_length, -1)
-    if use_beam_search:
-        generated_text_prefix = generate_beam(model, tokenizer, embed=prefix_embed)[0]
-    else:
-        generated_text_prefix = generate2(model, tokenizer, embed=prefix_embed)
-
-    return generated_text_prefix
+def get_image_caption(image_url):
+    return str(abs(hash(image_url)) % (10 ** 8))
